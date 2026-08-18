1e19843a08da9ceeb7e276310668129ab928ddf3
Roshab Bhandari <roshabbhandari1334@gmail.com>
2026-08-18 20:26:57 +0545

    feat: add shared CLI constants

---
+++ core/cli_constants.py
@@ -0,0 +1,6 @@
+"""Shared constants for the command-line toolkit."""
+
+APP_NAME = "Cybersec-Toolkit"
+APP_VERSION = "2.3"
+DEFAULT_HASH_ALGORITHM = "sha256"
+SUPPORTED_HASH_ALGORITHMS = ("md5", "sha1", "sha256", "sha512")
